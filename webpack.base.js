b76e39b645b3ac9fc2ca99fee9bbdbcf3960cd04
wclimb <875246904@qq.com>
2018-12-02 21:00:00 +0800

    update 更新代码

--- webpack.base.js
+++ webpack.base.js
@@ -4,40 +4,40 @@ const htmlPlugin = require('./config/html');
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    entry,
-    output:{
-        filename: 'js/[name].[hash].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        ...htmlPlugin,
-        new OptimizeCSSPlugin({
-            cssProcessorOptions: {
-                safe: true
-            }
-        }),
-    ],
-    performance: {
-        hints: false
-    },
-    module:{
-        rules:[
-            {
-                test: /\.ejs$/,
-                loader: 'underscore-template-loader',
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 10000,
-                    name: path.posix.join('./','img/[name].[hash:7].[ext]')
-                }
-            },
-        ]
-    },
+  entry,
+  output: {
+    filename: 'js/[name].[hash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    ...htmlPlugin,
+    new OptimizeCSSPlugin({
+      cssProcessorOptions: {
+        safe: true
+      }
+    }),
+  ],
+  performance: {
+    hints: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ejs$/,
+        loader: 'underscore-template-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: path.posix.join('./', 'img/[name].[hash:7].[ext]')
+        }
+      },
+    ]
+  },
 }
